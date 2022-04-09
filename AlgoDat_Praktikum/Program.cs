f7c0574df384a2c31f0a8bcec95d45fde091b2a0
david <davidgit@gmx.de>
2022-04-09 17:13:08 +0200

    first template

---
+++ AlgoDat_Praktikum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AlgoDat_Praktikum.Code;
+
+namespace AlgoDat_Praktikum
+{
+    /// <summary>
+    /// Only use to interact with user and its inputs.
+    /// Own implementations are handled in own classes.
+    /// If you add code here create an own region and consider merge conflicts.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
