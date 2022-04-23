8f9f9f40fb6434ba00388b7290b026da68c34018
david <87530266+davidg-h@users.noreply.github.com>
2022-04-23 17:57:23 +0200

    Update Helper.cs

--- AlgoDat_Praktikum/Code/abstrDataType/Helper.cs
+++ AlgoDat_Praktikum/Code/abstrDataType/Helper.cs
@@ -10,13 +10,15 @@ namespace AlgoDat_Praktikum.Code.abstrDataType
         /// else successfulInsert => false and elemPos => index (where elem should be inserted)
         /// </summary>
         private (bool successfulInsert, int elemPos) searchHelper;
-
+        
+        // has to be private when implemented s.r.: Praktikums-Aufgabestellung S.12 (delete this comment after implementation)
+        // or implementation with variables s.r.: Praktikums-Aufgabestellung S.12 and searchHelper (decision on one method)
         /// <summary>
         /// helper for search
         /// </summary>
         /// <param name="elem"></param>
         /// <returns></returns>
-        public abstract (bool, int) _search(int elem); //has to be private when implemented s.r.: Praktikums-Aufgabestellung S.12 (delete this comment after implementation)
+        public abstract (bool, int) _search(int elem); 
 
         public virtual bool search(int elem)
         {
