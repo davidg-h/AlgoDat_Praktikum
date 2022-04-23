09567b62ff100d53a537cd741dd50e62f8a64719
david <87530266+davidg-h@users.noreply.github.com>
2022-04-23 17:50:03 +0200

    Update Program.cs

--- AlgoDat_Praktikum/Program.cs
+++ AlgoDat_Praktikum/Program.cs
@@ -16,7 +16,7 @@ namespace AlgoDat_Praktikum
             switch (userInput)
             {
                 case "1":
-                    // TODO: let your class handle the console inputs and error handling (no negative integers)
+                    // TODO: let your class handle the console inputs and error handling (only not negative numbers)
                     // for more legible code in the main after user enters a concrete data type (s.r.: example)
                     Console.WriteLine("Wähle den konkreten Datentyp aus");
                     Console.WriteLine("a (Array)");
