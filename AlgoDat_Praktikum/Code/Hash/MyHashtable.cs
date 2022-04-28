728107e8e975f44420c4b8fe96138cad99e32015
Firevince <vneu.fire@gmail.com>
2022-04-28 10:09:00 +0200

    fixed errors

--- AlgoDat_Praktikum/Code/Hash/MyHashtable.cs
+++ AlgoDat_Praktikum/Code/Hash/MyHashtable.cs
@@ -14,7 +14,11 @@ namespace AlgoDat_Praktikum.Code.Array
 
         public MyHashtable(int size){
             this.size = size;
-            memory = new String[size];
+            memory = new int[size];
+            for (int i = 0; i < memory.Length; i++)
+            {
+                memory[i] = -1;
+            }
         }
         public bool search(int elem){
             for (int i = 0; i < size; i++)
@@ -27,24 +31,24 @@ namespace AlgoDat_Praktikum.Code.Array
         public bool insert(int elem, int key = -1)
         {
             int i = 0;
-            while(i < size && memory[i] != "" ) i++;
+            while(i < size && memory[i] != -1 ) i++;
             if(i == size) return false;
             memory[i] = elem;
             return true;
         }
         public bool insert2(int key,int elem){
             int newKey = divisionRestMethode(key);
-            if(memory[newKey] == ""){
-                memory[newKey] == elem;
+            if(memory[newKey] == -1){
+                memory[newKey] = elem;
                 return true;
             }
             else{
                 int i = 1;
-                while(newKey-i >= 0 && memory[newKey-i] != "") i++;
+                while(newKey-i >= 0 && memory[newKey-i] != -1) i++;
                 if(i < 0) {
-                    while(newKey+i <= size && memory[newKey+i] != "") i++;
+                    while(newKey+i <= size && memory[newKey+i] != -1) i++;
                     if(i > 0) {
-                    while(newKey-i >= 0 && memory[newKey-i] != "") i++;
+                    while(newKey-i >= 0 && memory[newKey-i] != -1) i++;
                 }
                 else{
                     memory[newKey-i] = elem;
