084487938bf1473f2fad5893eb382fe50b30ef6e
DavidNguyen <davidGit@gmx.de>
2022-06-15 19:25:26 +0200

    added class diagram generated from the code

--- AlgoDat_Praktikum/Code/Array/MultiSetSortedArray.cs
+++ AlgoDat_Praktikum/Code/Array/MultiSetSortedArray.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlgoDat_Praktikum.Code.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace AlgoDat_Praktikum.Code.Array_
 {
-    class MultiSetSortedArray : BaseArraySorted
+    class MultiSetSortedArray : BaseArraySorted, IMultiSetSorted<int>
     {
         public MultiSetSortedArray(int size) : base(size) { }
 
